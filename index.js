8cff8ba4c47585ed046f15ca59aa103aa3255969
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-07 12:16:23 -0600

    commit tree 0e2d69878f8afcec517fccc6fdfe7bb4fdb8c5bb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr) {
+    const obj = arr.find(element => element.result === "W");
+    if (!obj) {
+        return undefined;
+    } else {
+        return obj.year;
+    }
+}
